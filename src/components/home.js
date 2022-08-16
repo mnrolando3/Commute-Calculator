00f02e2ad3d7ab1bd13de6b33710f1871af5ffba
moperry2 <moperrry@gmail.com>
2022-08-16 09:41:34 -0400

    added styling changes to map input

--- src/components/home.js
+++ src/components/home.js
@@ -3,9 +3,9 @@ import { Input, ChakraProvider, theme } from "@chakra-ui/react";
 import React, { useState, useEffect, useRef } from "react";
 import { Autocomplete } from "@react-google-maps/api";
 import Map from "./map";
+import "./Home.css";
 
 export default function Home() {
-
   const originRef = useRef();
   const destinationRef = useRef();
   const yearsArray = [
@@ -17,13 +17,13 @@ export default function Home() {
   const [mpgInput, setMpgInput] = useState(null);
   const [selectYear, setSelectYear] = useState(1991);
   const [carMakes, setCarMakes] = useState([]);
-  const [carMakeID, setCarMakeID] = useState('1')
+  const [carMakeID, setCarMakeID] = useState("1");
   const [workDay, setWorkDay] = useState(1);
-  const [startPoint, setStartPoint] = useState('')
-  const [endPoint, setEndPoint] = useState('')
-  const [carModels, setCarModels] = useState([])
-  const [carTrimID, setCarTrimID] = useState('')
-  const [combinedMPGVal, setCombinedMPGVal] = useState('')
+  const [startPoint, setStartPoint] = useState("");
+  const [endPoint, setEndPoint] = useState("");
+  const [carModels, setCarModels] = useState([]);
+  const [carTrimID, setCarTrimID] = useState("");
+  const [combinedMPGVal, setCombinedMPGVal] = useState("");
 
   useEffect(() => {
     axios
@@ -33,65 +33,80 @@ export default function Home() {
         setCarMakes(res.data);
       });
   }, []);
-  
-  
-    useEffect(() => {
-        if (selectYear && carMakeID) {
-            axios
-                .get(`https://fathomless-mountain-86819.herokuapp.com/getvehiclemodels?year=${selectYear}&makeid=${carMakeID}`)
-                .then((res) => {
-                    console.log('models', res)
-                    setCarModels(res.data)
-                })
-        }
-    }, [selectYear, carMakeID])
 
-    useEffect(() => {
-        if (selectYear && carTrimID) {
-            axios
-                .get(`https://fathomless-mountain-86819.herokuapp.com/getvehiclespec?year=${selectYear}&trimid=${carTrimID}`)
-                .then((res) => {
-                    console.log('trims', res.data)
-                    setCombinedMPGVal(res.data.CombinedMpg)
-                })
-        }
-    }, [selectYear, carTrimID])
-    
-    const handleAskQuestion = (event) => {
-        event.preventDefault()
-        console.log('starting point:', startPoint)
-        console.log('ending point:', endPoint)
-        console.log('year:', selectYear)
-        console.log('car make id:', carMakeID)
-        console.log('car trim id:', carTrimID)
-        // console.log('mpg input:', mpgInput)
-        console.log('work day:', workDay)
+  useEffect(() => {
+    if (selectYear && carMakeID) {
+      axios
+        .get(
+          `https://fathomless-mountain-86819.herokuapp.com/getvehiclemodels?year=${selectYear}&makeid=${carMakeID}`
+        )
+        .then((res) => {
+          console.log("models", res);
+          setCarModels(res.data);
+        });
     }
+  }, [selectYear, carMakeID]);
+
+  useEffect(() => {
+    if (selectYear && carTrimID) {
+      axios
+        .get(
+          `https://fathomless-mountain-86819.herokuapp.com/getvehiclespec?year=${selectYear}&trimid=${carTrimID}`
+        )
+        .then((res) => {
+          console.log("trims", res.data);
+          setCombinedMPGVal(res.data.CombinedMpg);
+        });
+    }
+  }, [selectYear, carTrimID]);
+
+  const handleAskQuestion = (event) => {
+    event.preventDefault();
+    console.log("starting point:", startPoint);
+    console.log("ending point:", endPoint);
+    console.log("year:", selectYear);
+    console.log("car make id:", carMakeID);
+    console.log("car trim id:", carTrimID);
+    // console.log('mpg input:', mpgInput)
+    console.log("work day:", workDay);
+  };
 
   return (
     <ChakraProvider them={theme}>
       <div>
         <form onSubmit={handleAskQuestion}>
-          <div>
-            <label htmlFor="starting-location-field">Starting Location: </label>
-            <Autocomplete>
-              <Input type="text" placeholder="Origin" ref={originRef} />
-            </Autocomplete>
-          </div>
-          <br />
+          <div className="map-container">
+            <div className="row">
+              <div className="left-panel box">
+                <div>
+                  <label htmlFor="starting-location-field">
+                    Starting Location:{" "}
+                  </label>
+                  <Autocomplete>
+                    <Input type="text" placeholder="Origin" ref={originRef} />
+                  </Autocomplete>
+                </div>
+                <br />
 
-          <div>
-            <label htmlFor="ending-location-field">Ending Location: </label>
-            <Autocomplete>
-              <Input
-                type="text"
-                placeholder="Destination"
-                ref={destinationRef}
-              />
-            </Autocomplete>
+                <div>
+                  <label htmlFor="ending-location-field">
+                    Ending Location:{" "}
+                  </label>
+                  <Autocomplete>
+                    <Input
+                      type="text"
+                      placeholder="Destination"
+                      ref={destinationRef}
+                    />
+                  </Autocomplete>
+                </div>
+              </div>
+              <div className="right-panel box">
+                <Map originRef={originRef} destinationRef={destinationRef} />
+              </div>
+              <br />
+            </div>
           </div>
-          <br />
-
           <div>
             <label htmlFor="year-field">Year: </label>
             <select
@@ -108,48 +123,47 @@ export default function Home() {
           </div>
           <br />
           <div>
-             <label htmlFor='car-make-field'>Car Make: </label>
-                <select
-                    id='car-make-field'
-                    onChange={(e) => setCarMakeID(e.target.value)}
-                >
-                    {carMakes.map((makez, index) => (
-                        <option key={index} value={makez.Id}>
-                            {makez.Name}
-                        </option>
-                    ))}
-                </select>
-            </div>
-          <br />
-          
-            <div>
-              <label htmlFor='car-model-field'>Car Model: </label>
-              <select
-                  id='car-model-field'
-                  onChange={(e) => setCarTrimID(e.target.value)}
-              >
-                  {carModels.map((modelz, index) => (
-                      <option key={index} value={modelz.TrimId}>
-                          {modelz.TrimName}
-                      </option>
-                  ))}
-              </select>
+            <label htmlFor="car-make-field">Car Make: </label>
+            <select
+              id="car-make-field"
+              onChange={(e) => setCarMakeID(e.target.value)}
+            >
+              {carMakes.map((makez, index) => (
+                <option key={index} value={makez.Id}>
+                  {makez.Name}
+                </option>
+              ))}
+            </select>
           </div>
           <br />
-
 
           <div>
-              <label htmlFor='mpg-input-field'>Input MPG: </label>
-              <input
-                  id='mpg-input-field'
-                  type="text"
-                  value={combinedMPGVal}
-                  onChange={(e) => setCombinedMPGVal(e.target.value)}
-                  required
-              />
+            <label htmlFor="car-model-field">Car Model: </label>
+            <select
+              id="car-model-field"
+              onChange={(e) => setCarTrimID(e.target.value)}
+            >
+              {carModels.map((modelz, index) => (
+                <option key={index} value={modelz.TrimId}>
+                  {modelz.TrimName}
+                </option>
+              ))}
+            </select>
           </div>
-          <br />   
-   
+          <br />
+
+          <div>
+            <label htmlFor="mpg-input-field">Input MPG: </label>
+            <input
+              id="mpg-input-field"
+              type="text"
+              value={combinedMPGVal}
+              onChange={(e) => setCombinedMPGVal(e.target.value)}
+              required
+            />
+          </div>
+          <br />
+
           <div>
             <label htmlFor="work-days-field">Working Days: </label>
             <select
@@ -171,7 +185,6 @@ export default function Home() {
           </div>
         </form>
       </div>
-      <Map originRef={originRef} destinationRef={destinationRef} />
     </ChakraProvider>
   );
 }
