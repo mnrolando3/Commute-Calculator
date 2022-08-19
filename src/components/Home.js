3a249ba438c35711a9f7f13fe28518afcd4e326d
moperry2 <moperrry@gmail.com>
2022-08-19 13:51:46 -0400

    updated the user input form

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,11 +9,11 @@ import {
   HStack,
   GridItem,
   Grid,
-} from '@chakra-ui/react'
-import React, { useState, useEffect, useRef } from 'react'
-import { Autocomplete } from '@react-google-maps/api'
+} from "@chakra-ui/react";
+import React, { useState, useEffect, useRef } from "react";
+import { Autocomplete } from "@react-google-maps/api";
 
-import Map from './Map'
+import Map from "./Map";
 import {
   createCalcData,
   createCommute,
@@ -22,146 +22,146 @@ import {
   getGasPrice,
   getMakes,
   getVehicleSpecs,
-} from '../utils/api'
-import { roundNumber, splitAddress } from '../utils/helpers'
-import { YEARS, WORK_DAYS } from '../utils/constants'
+} from "../utils/api";
+import { roundNumber, splitAddress } from "../utils/helpers";
+import { YEARS, WORK_DAYS } from "../utils/constants";
 
 export default function Home() {
-  const originRef = useRef()
-  const destinationRef = useRef()
-  const [selectYear, setSelectYear] = useState(2020)
-  const [carMakes, setCarMakes] = useState([])
-  const [carMakeID, setCarMakeID] = useState('1')
-  const [workDay, setWorkDay] = useState(1)
-  const [carModels, setCarModels] = useState([])
-  const [carTrimID, setCarTrimID] = useState('')
-  const [combinedMPGVal, setCombinedMPGVal] = useState('')
-  const [distance, setDistance] = useState('')
-  const [duration, setDuration] = useState('')
-  const [directionsResponse, setDirectionsResponse] = useState(null)
+  const originRef = useRef();
+  const destinationRef = useRef();
+  const [selectYear, setSelectYear] = useState(2020);
+  const [carMakes, setCarMakes] = useState([]);
+  const [carMakeID, setCarMakeID] = useState("1");
+  const [workDay, setWorkDay] = useState(1);
+  const [carModels, setCarModels] = useState([]);
+  const [carTrimID, setCarTrimID] = useState("");
+  const [combinedMPGVal, setCombinedMPGVal] = useState("");
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+  const [directionsResponse, setDirectionsResponse] = useState(null);
   const [resultCalculation, setResultCalculation] = useState({
-    result: { weekly: '' },
-  })
+    result: { weekly: "" },
+  });
 
   useEffect(() => {
     const getMakesAsync = async () => {
-      const makes = await getMakes()
-      setCarMakes(makes)
-    }
-    getMakesAsync()
-  }, [])
+      const makes = await getMakes();
+      setCarMakes(makes);
+    };
+    getMakesAsync();
+  }, []);
 
   useEffect(() => {
     if (selectYear && carMakeID) {
       const getCarModelsAsync = async () => {
-        const result = await getCarModels(selectYear, carMakeID)
+        const result = await getCarModels(selectYear, carMakeID);
         if (result.data.length === 0) {
-          setCombinedMPGVal(0)
+          setCombinedMPGVal(0);
         }
-        setCarModels(result.data)
-      }
-      getCarModelsAsync()
+        setCarModels(result.data);
+      };
+      getCarModelsAsync();
     }
-  }, [selectYear, carMakeID])
+  }, [selectYear, carMakeID]);
 
   useEffect(() => {
     if (selectYear && carTrimID) {
       const getMpg = async () => {
-        const mpgValueData = await getVehicleSpecs(selectYear, carTrimID)
+        const mpgValueData = await getVehicleSpecs(selectYear, carTrimID);
         if (mpgValueData) {
-          const roundedMPGVal = roundNumber(mpgValueData)
-          setCombinedMPGVal(roundedMPGVal)
+          const roundedMPGVal = roundNumber(mpgValueData);
+          setCombinedMPGVal(roundedMPGVal);
         } else {
-          setCombinedMPGVal(0.0)
+          setCombinedMPGVal(0.0);
         }
-      }
-      getMpg()
+      };
+      getMpg();
     }
-  }, [selectYear, carTrimID])
+  }, [selectYear, carTrimID]);
 
   async function calculateRoute() {
-    if (originRef.current.value === '' || destinationRef.current.value === '') {
-      return
+    if (originRef.current.value === "" || destinationRef.current.value === "") {
+      return;
     }
     // eslint-disable-next-line no-undef
-    const directionsService = new google.maps.DirectionsService()
+    const directionsService = new google.maps.DirectionsService();
     const results = await directionsService.route({
       origin: originRef.current.value,
       destination: destinationRef.current.value,
       // eslint-disable-next-line no-undef
       travelMode: google.maps.TravelMode.DRIVING,
-    })
-    const distanceResult = results.routes[0].legs[0].distance.text
-    setDirectionsResponse(results)
-    setDuration(results.routes[0].legs[0].duration.text)
-    setDistance(distanceResult)
-    return distanceResult
+    });
+    const distanceResult = results.routes[0].legs[0].distance.text;
+    setDirectionsResponse(results);
+    setDuration(results.routes[0].legs[0].duration.text);
+    setDistance(distanceResult);
+    return distanceResult;
   }
 
   const commutePostData = async (distanceValue) => {
-    let cityStart = splitAddress(originRef.current.value)
-    let cityEnd = splitAddress(destinationRef.current.value)
-    const startAvgGasLocation = await getGasPrice(cityStart)
-    const endAvgGasLocation = await getGasPrice(cityEnd)
-    const startGas = startAvgGasLocation.data.locationAverage
-    const endGas = endAvgGasLocation.data.locationAverage
+    let cityStart = splitAddress(originRef.current.value);
+    let cityEnd = splitAddress(destinationRef.current.value);
+    const startAvgGasLocation = await getGasPrice(cityStart);
+    const endAvgGasLocation = await getGasPrice(cityEnd);
+    const startGas = startAvgGasLocation.data.locationAverage;
+    const endGas = endAvgGasLocation.data.locationAverage;
 
-    const avgGasLocation = roundNumber((startGas + endGas) / 2)
+    const avgGasLocation = roundNumber((startGas + endGas) / 2);
     const response = await createCommute(
       cityStart,
       cityEnd,
       workDay,
       distanceValue,
       avgGasLocation
-    )
-    return response.data.id
-  }
+    );
+    return response.data.id;
+  };
 
   const calculateData = async (e) => {
-    e.preventDefault()
-    let [resultDistance] = await Promise.all([calculateRoute()])
+    e.preventDefault();
+    let [resultDistance] = await Promise.all([calculateRoute()]);
     let [commuteId, vehicleId] = await Promise.all([
       commutePostData(resultDistance),
       createVehicle(combinedMPGVal),
-    ])
-    const resultCalcData = await createCalcData(commuteId, vehicleId)
-    setResultCalculation(resultCalcData)
-  }
+    ]);
+    const resultCalcData = await createCalcData(commuteId, vehicleId);
+    setResultCalculation(resultCalcData);
+  };
 
   return (
     <CharkhaProvider theme={theme}>
       <Flex
-        position='relative'
-        flexDirection='column'
-        alignItems='center'
-        w='100vw'
+        position="relative"
+        flexDirection="column"
+        alignItems="center"
+        w="100vw"
       >
         <form onSubmit={calculateData}>
           {/* stat location input  */}
-          <div style={{ paddingBottom: '40px' }}>
-            <label htmlFor='starting-location-field'>Starting Location: </label>
+          <div style={{ paddingBottom: "40px" }}>
+            <label htmlFor="starting-location-field">Starting Location: </label>
             <Autocomplete>
-              <Input type='text' placeholder='Origin' ref={originRef} />
+              <Input type="text" placeholder="Origin" ref={originRef} />
             </Autocomplete>
           </div>
 
           {/* end location input  */}
-          <div style={{ paddingBottom: '50px' }}>
-            <label htmlFor='ending-location-field'>Ending Location: </label>
+          <div style={{ paddingBottom: "50px" }}>
+            <label htmlFor="ending-location-field">Ending Location: </label>
             <Autocomplete>
               <Input
-                type='text'
-                placeholder='Destination'
+                type="text"
+                placeholder="Destination"
                 ref={destinationRef}
               />
             </Autocomplete>
           </div>
 
           {/* days per week dp  */}
-          <div style={{ paddingBottom: '50px' }}>
-            <label htmlFor='work-days-field'>Days per Week Commuting: </label>
+          <div style={{ paddingBottom: "50px" }}>
+            <label htmlFor="work-days-field">Days per Week Commuting: </label>
             <select
-              id='work-days-field'
+              id="work-days-field"
               defaultValue=""
               onChange={(e) => setWorkDay(e.target.value)}
             >
@@ -176,15 +176,48 @@ export default function Home() {
             </select>
           </div>
 
-          <div style={{ paddingBottom: '20px' }}>
-            <p><b>Select Vehicle Information</b></p>
+          <div style={{ paddingBottom: "20px" }}>
+            <p>
+              <b>Input MPG Value</b>
+            </p>
+          </div>
+
+          <div style={{ paddingBottom: "40px" }}>
+            {carModels.length === 0 ? (
+              <p>No models found, please enter MPG</p>
+            ) : combinedMPGVal === 0.0 ? (
+              <p>No MPG found, please enter MPG</p>
+            ) : (
+              ""
+            )}
+            {combinedMPGVal === 0.0 && <p>No MPG found, please enter MPG</p>}
+            <label htmlFor="mpg-input-field">Combined MPG: </label>
+            <input
+              id="mpg-input-field"
+              type="text"
+              value={combinedMPGVal}
+              onChange={(e) => setCombinedMPGVal(e.target.value)}
+              required
+            />
+          </div>
+
+          <div style={{ paddingBottom: "40px" }}>
+            <p>
+              <b>OR</b>
+            </p>
+          </div>
+
+          <div style={{ paddingBottom: "20px" }}>
+            <p>
+              <b>Select Vehicle Information to Auto-Populate MPG Value</b>
+            </p>
           </div>
 
           {/* year dp  */}
-          <div style={{ paddingBottom: '40px' }}>
-            <label htmlFor='year-field'>Year: </label>
+          <div style={{ paddingBottom: "40px" }}>
+            <label htmlFor="year-field">Year: </label>
             <select
-              id='year-field'
+              id="year-field"
               defaultValue=""
               onChange={(e) => setSelectYear(e.target.value)}
             >
@@ -201,10 +234,10 @@ export default function Home() {
           </div>
 
           {/* car make dp  */}
-          <div style={{ paddingBottom: '40px' }}>
-            <label htmlFor='car-make-field'>Car Make: </label>
+          <div style={{ paddingBottom: "40px" }}>
+            <label htmlFor="car-make-field">Car Make: </label>
             <select
-              id='car-make-field'
+              id="car-make-field"
               defaultValue=""
               onChange={(e) => setCarMakeID(e.target.value)}
             >
@@ -220,10 +253,10 @@ export default function Home() {
           </div>
 
           {/* car model dp  */}
-          <div style={{ paddingBottom: '40px' }}>
-            <label htmlFor='car-model-field'>Car Model: </label>
+          <div style={{ paddingBottom: "40px" }}>
+            <label htmlFor="car-model-field">Car Model: </label>
             <select
-              id='car-model-field'
+              id="car-model-field"
               defaultValue=""
               onChange={(e) => setCarTrimID(e.target.value)}
             >
@@ -242,41 +275,14 @@ export default function Home() {
             </select>
           </div>
 
-          <div style={{ paddingBottom: '40px' }}>
-            <p><b>OR</b></p>
-          </div>
-
-          <div style={{ paddingBottom: '20px' }}>
-            <p><b>Input MPG Value</b></p>
-          </div>
-
-          <div style={{ paddingBottom: '40px' }}>
-            {carModels.length === 0 ? (
-              <p>No models found, please enter MPG</p>
-            ) : combinedMPGVal === 0.0 ? (
-              <p>No MPG found, please enter MPG</p>
-            ) : (
-              ''
-            )}
-            {combinedMPGVal === 0.0 && <p>No MPG found, please enter MPG</p>}
-            <label htmlFor='mpg-input-field'>Combined MPG: </label>
-            <input
-              id='mpg-input-field'
-              type='text'
-              value={combinedMPGVal}
-              onChange={(e) => setCombinedMPGVal(e.target.value)}
-              required
-            />
-          </div>
-
           <div>
             <HStack
-              style={{ paddingBottom: '40px' }}
+              style={{ paddingBottom: "40px" }}
               spacing={4}
               mt={4}
-              justifyContent='space-between'
+              justifyContent="space-between"
             >
-              <Button colorScheme='blue' type='submit'>
+              <Button colorScheme="blue" type="submit">
                 Commutilate Route
               </Button>
               <Text>Distance: {distance} </Text>
@@ -288,8 +294,8 @@ export default function Home() {
       {/* end form  */}
 
       {/* map  */}
-      <div className='map-container'>
-        <Grid templateColumns='repeat(5, 1fr)' gap={4}>
+      <div className="map-container">
+        <Grid templateColumns="repeat(5, 1fr)" gap={4}>
           <GridItem colSpan={2}>
             <Map
               distance={distance}
@@ -303,11 +309,11 @@ export default function Home() {
           {/* result  */}
           <GridItem colStart={4} colEnd={6}>
             {resultCalculation.result.weekly > 0 ? (
-              <Center w='300px' h='500px'>
+              <Center w="300px" h="500px">
                 <Text>Weekly Results: ${resultCalculation.result.weekly}</Text>
               </Center>
             ) : (
-              <Center w='300px' h='500px'>
+              <Center w="300px" h="500px">
                 <Text>
                   Please enter your car information to get the weekly result.
                 </Text>
@@ -317,5 +323,5 @@ export default function Home() {
         </Grid>
       </div>
     </CharkhaProvider>
-  )
+  );
 }
