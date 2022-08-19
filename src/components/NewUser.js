1d260c620a119a540e7d26c4afdc8e2241e0db16
Jose Reyes <jreyesri@cbu.edu>
2022-08-19 13:40:45 -0400

    Worked on new user page

---
+++ src/components/NewUser.js
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+
+
+export const NewUserForm = () => {
+
+    const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(email)
+        console.log(username)
+        console.log(password)
+
+
+    }
+
+    return (
+        <>
+            <div className="wrap">
+                <h2>Create an Account</h2>
+                <form id="login-form" onSubmit={handleSubmit}>
+                    <div className="controls">
+                        <label htmlFor="email-field">email: </label>
+                        <input
+                            id="email-field"
+                            onChange={(e) => setEmail(e.target.value)}
+                            type="text"
+                        />
+                    </div>
+                    <div className="controls">
+                        <label htmlFor="username-field">username: </label>
+                        <input
+                            id="username-field"
+                            onChange={(e) => setUsername(e.target.value)}
+                            type="text"
+                        />
+                    </div>
+                    <div className="controls">
+                        <label htmlFor="password-field">password: </label>
+                        <input
+                            id="password-field"
+                            onChange={(e) => setPassword(e.target.value)}
+                            type="password"
+                        />
+                    </div>
+                    <div className="form-submit">
+                        <input type="submit" value="Create Account" className="button" />
+                    </div>
+                </form>
+            </div>
+        </>
+    );
+}
+
+export default NewUserForm;
